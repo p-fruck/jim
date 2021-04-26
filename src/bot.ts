52836503da8122c2c23b15d059bbea0430617216
Philipp <dev@p-fruck.de>
2021-04-26 15:59:10 +0200

    code refactoring

--- src/bot.ts
+++ src/bot.ts
@@ -2,46 +2,35 @@ import { Browser, Page } from 'puppeteer-core';
 import youtubedl, { YtResponse } from 'youtube-dl-exec';
 import Mutex from './mutex';
 import config from './config';
+import { IIncomingMessage, IParticipantKickedOut } from './models/jitsi.interface';
 
-const youtubeConf = {
-  dumpJson: true,
-  noWarnings: true,
-  noCallHome: true,
-  extractAudio: true,
-  audioFormat: 'vorbis',
-  noCheckCertificate: true,
-  preferFreeFormats: true,
-  defaultSearch: 'ytsearch',
-  youtubeSkipDashManifest: true,
-}
-
-interface IIncomingMessage {
-  from: string, // The id of the user that sent the message
-  nick: string, // the nickname of the user that sent the message
-  privateMessage: boolean, // whether this is a private or group message
-  message: string // the text of the message
-}
-
-interface IParticipantKickedOut {
-  kicked: {
-    id: string, // the id of the participant removed from the room
-    local: boolean // whether or not the participant is the local particiapnt
-  },
-  kicker: {
-    id: string // the id of the participant who kicked out the other participant
-  }
-}
-
+// eslint-disable-next-line no-unused-vars
 type ExposableFunction = (arg0: any) => any;
 
-export class JitsiBot {
-  private queue = <YtResponse[]> [];
+export default class JitsiBot {
   private messageMutex = new Mutex();
 
-  private constructor(private page: Page) {
-    void this.exposeListenerFunction(this.participantKickedOut);
-    void this.exposeListenerFunction(this.videoConferenceJoined);
-    void this.exposeListenerFunction(this.onAudioEnded);
+  private page: Page;
+
+  private queue = <YtResponse[]> [];
+
+  private youtubeConf = {
+    dumpJson: true,
+    noWarnings: true,
+    noCallHome: true,
+    extractAudio: true,
+    audioFormat: 'vorbis',
+    noCheckCertificate: true,
+    preferFreeFormats: true,
+    defaultSearch: 'ytsearch',
+    youtubeSkipDashManifest: true,
+  };
+
+  private constructor(page: Page) {
+    this.page = page;
+    this.exposeListenerFunction(this.participantKickedOut);
+    this.exposeListenerFunction(this.videoConferenceJoined);
+    this.exposeListenerFunction(this.onAudioEnded);
   }
 
   /**
@@ -59,7 +48,7 @@ export class JitsiBot {
     const url = `file://${__dirname}/../index.html`;
 
     await page.goto(url, { waitUntil: 'load' });
-    const gain = .01 * config.volume.initialValue;
+    const gain = config.volume.initialValue / 100;
     await page.evaluate(`joinConference('${roomName}', '${botName}', ${gain})`);
 
     return new JitsiBot(page);
@@ -89,11 +78,11 @@ export class JitsiBot {
       case '!add':
         if (params.length) {
           if (this.queue.length >= config.playlist.maxSize) {
-            void this.sendMessage(`Sorry, I cannot remember more than ${config.playlist.maxSize} tracks :confounded_face:`)
+            this.sendMessage(`Sorry, I cannot remember more than ${config.playlist.maxSize} tracks :confounded_face:`);
           }
           const track = await this.fetchAudio(params.join(' '));
           this.queue.push(track);
-          if(await this.page.evaluate('audio.ended || audio.currentTime === 0')) {
+          if (await this.page.evaluate('audio.ended || audio.currentTime === 0')) {
             await this.onAudioEnded();
           }
         }
@@ -102,7 +91,7 @@ export class JitsiBot {
         this.queue = [];
         break;
       case '!help':
-        void this.sendMessages([
+        this.sendMessages([
           '!add <url|searchTerm> - Add track to queue',
           '!clear - Clear the queue',
           '!help - Print the help menu',
@@ -114,7 +103,7 @@ export class JitsiBot {
         ]);
         break;
       case '!list':
-        void this.sendMessages(this.queue.map((track) => track.title));
+        this.sendMessages(this.queue.map((track) => track.title));
         break;
       case '!pause':
         await this.page.evaluate('void audio.pause()');
@@ -130,34 +119,39 @@ export class JitsiBot {
           await this.playAudio(track);
         }
         break;
-      case '!vol':
+      case '!vol': {
         let gain = <number> await this.page.evaluate('getGain()');
         if (params.length === 0) {
-          void this.sendMessage(`Current volume level equals ${gain}%`);
+          this.sendMessage(`Current volume level equals ${gain}%`);
         } else {
           const { stepSize } = config.volume;
-          switch(true) {
+          switch (true) {
             case /^(0|100|[1-9][0-9]?)$/.test(params[0]):
               gain = parseInt(params[0], 10);
               break;
             case /^\++$/.test(params[0]):
               gain += params[0].length * stepSize;
               break;
-            case /^\-+$/.test(params[0]):
+            case /^-+$/.test(params[0]):
               gain -= params[0].length * stepSize;
               break;
             default:
-              void this.sendMessage(
-                'I did not understand that. Please use !vol to retrieve some ' +
-                'volume information, !vol [0-100] to set the volume level ' +
-                'directly or !vol (+|-), where each plus or minus increments/' +
-                `decrements the total gain by ${stepSize}`
+              this.sendMessage(
+                'I did not understand that. Please use !vol to retrieve some '
+                + 'volume information, !vol [0-100] to set the volume level '
+                + 'directly or !vol (+|-), where each plus or minus increments/'
+                + `decrements the total gain by ${stepSize}`,
               );
               return;
           }
-          this.page.evaluate(`setGain(${gain})`)
+          this.page.evaluate(`setGain(${gain})`);
         }
         break;
+      }
+      default:
+        if (cmd.startsWith('!')) {
+          this.sendMessage('Are you talking to me? :thinking: Try !help :bulb:');
+        }
     }
   }
 
@@ -183,39 +177,80 @@ export class JitsiBot {
     await this.page.browser().close(); // ToDo: Consider multiple bots per browser
   }
 
+  /**
+   * Expose a function to the frontend
+   *
+   * @param fn - The function to expose
+   */
   private async exposeFunction(fn: ExposableFunction): Promise<void> {
     await this.page.exposeFunction(fn.name, fn.bind(this));
   }
+
+  /**
+   * Add event listener to the jitsi api.
+   *
+   * @param fn - The function to add the listener to
+   */
   private async addEventListener(fn: ExposableFunction): Promise<void> {
     await this.page.evaluate(`api.addListener('${fn.name}', ${fn.name})`);
   }
+
+  /**
+   * Remove event listener from jitsi api
+   *
+   * @param fn - The function that has the listener registered
+   * @param name - The name of the listening function to remove
+   */
   private async removeEventListener(fn: ExposableFunction, name: string): Promise<void> {
     await this.page.evaluate(`api.removeListener('${fn.name}', ${name})`);
   }
+
+  /**
+   * Shortcut to expose a function and add an event listener
+   *
+   * @param fn - The function to expose
+   */
   private async exposeListenerFunction(fn: ExposableFunction): Promise<void> {
     await this.exposeFunction(fn);
     await this.addEventListener(fn);
   }
 
+  /**
+   * Convert a video url or a youtube search term to an Object
+   * containing information about the corresponding youtube video
+   *
+   * @param {string} input - The video url or youtube search term
+   * @returns {Promise<YtResponse>} - Information about the corresponding video
+   */
   async fetchAudio(input: string): Promise<YtResponse> {
-    return youtubedl(input, youtubeConf);
+    return youtubedl(input, this.youtubeConf);
   }
 
+  /**
+   * Start playback of a given track
+   *
+   * @param {YtResponse} track - The track to play
+   */
   async playAudio(track: YtResponse): Promise<void> {
     const opus = track.formats.filter((format) => format.acodec === 'opus');
 
     if (!opus.length) {
       throw new Error('Couldn\'t play video due to nonfree codec');
     }
-    void this.sendMessage(`:notes: Playing ${track.title}`)
-    void this.setAvatarUrl(track.thumbnail);
-    try  {
+    this.sendMessage(`:notes: Playing ${track.title}`);
+    this.setAvatarUrl(track.thumbnail);
+    try {
       await this.page.evaluate(`playAudio('${opus[0].url}')`);
     } catch (err) {
-      console.error('Failed to play audio', track.url, opus[0], err);
+      throw new Error(`Failed to play audio - ${track.url},  ${err}`);
     }
   }
 
+  /**
+   * Sends a chat message
+   *
+   * @param {string} msg - The message to send
+   */
   async sendMessage(msg: string): Promise<void> {
     await this.page.waitForSelector('iframe');
     const elementHandle = await this.page.$('iframe');
@@ -227,22 +262,35 @@ export class JitsiBot {
     unlock();
   }
 
+  /**
+   * Sends a multiline chat message
+   *
+   * @param {string[]} msgs - Lines to send
+   */
   async sendMessages(msgs: string[]): Promise<void> {
     await this.page.waitForSelector('iframe');
     const elementHandle = await this.page.$('iframe');
     const frame = await elementHandle.contentFrame();
 
     const unlock = await this.messageMutex.acquire();
+    // eslint-disable-next-line no-restricted-syntax
     for (const msg of msgs) {
+      /* eslint-disable no-await-in-loop */
       await frame.type('#usermsg', msg);
       await this.page.keyboard.down('Shift');
       await this.page.keyboard.press('Enter');
       await this.page.keyboard.up('Shift');
+      /* eslint-enable no-await-in-loop */
     }
     await frame.click('.send-button');
     unlock();
   }
 
+  /**
+   * Set avatar picture to a given image
+   *
+   * @param {string} url - Url to the image
+   */
   async setAvatarUrl(url: string): Promise<void> {
     await this.page.evaluate(`api.executeCommand('avatarUrl', '${url}')`);
   }
