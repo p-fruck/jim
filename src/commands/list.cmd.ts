4b12b5eb24893c89df08eb46d6d420982d95b383
Philipp <dev@p-fruck.de>
2021-04-27 16:22:51 +0200

    minor fixes

--- src/commands/list.cmd.ts
+++ src/commands/list.cmd.ts
@@ -3,7 +3,11 @@ import { IJimCommand } from '../command.service';
 
 export default <IJimCommand> {
   execute: async (jim: JitsiBot) => {
-    await jim.sendMessages(jim.queue.map((track) => track.title));
+    if (jim.queue.length === 0) {
+      await jim.sendMessage('No track in queue - Go ahead and add one :notes:');
+    } else {
+      await jim.sendMessages(jim.queue.map((track) => track.title));
+    }
   },
   description: 'Show tracks in queue',
 };
