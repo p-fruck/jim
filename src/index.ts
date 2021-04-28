d6f264f37d8ab1b8eb18bcec90fbc27643e761a1
Philipp <dev@p-fruck.de>
2021-04-28 17:06:45 +0200

    added fake microphone

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ async function startBrowser(): Promise<void> {
   const browser = await puppeteer.launch({
     args: [
       '--use-fake-ui-for-media-stream', // disable asking for webcam & video
+      '--use-fake-device-for-media-stream', // use fake microphone
       '--disable-web-security', // enable playback of cross origin media/ressources
       '--disable-features=IsolateOrigins,site-per-process', // allow to access cross-origin iframe
     ],
